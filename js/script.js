6847591c938410468a47e9dff37909962dcafa9c
ALR674 <212073B@mymail.nyp.edu.sg>
2022-08-03 10:06:33 +0800

    added transition to menu button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 //for the mobile & tablet drop down navigation menu
+var menuOpen = false; //used to decide what degree to rotate the menu icon to
 let menu = document.querySelector("#menu"); //find the menu icon img
 let dropDown = document.querySelector(".navi"); //find drop down menu
 menu.addEventListener("click", ()=> {
+    var rotationDegree = menuOpen ? 0 : 90; //if menu is open, rotate back to OG position
+        //else rotate to 90 degree
+    var scalingBy = menuOpen ? 1 : -0.5;
     dropDown.classList.toggle("hidden");
+    //menu.style.transform = 'rotate('+rotationDegree+'deg)';
+    menu.style.transform = 'scale('+scalingBy+','+scalingBy+')';
+    menuOpen = !menuOpen;
 });
 
 let opened; /* stores which pop up is opened, used to decide which pop up to close when the x close button of a pop up is pressed */
